49786110a3ae195764d74f6b18bffdd74eb65f13
RGBDeehan <rageebyasardeehan@gmail.com>
2024-12-17 13:33:38 +0600

    Update ArrayAssignment11.c

--- ArrayAssignment11.c
+++ ArrayAssignment11.c
@@ -4,16 +4,15 @@ Sample Input
 40 10 30 15 30 30 30
 Sample Output
 4 */
-
 #include <stdio.h>
 int main(){
-int i,x[5],a,b,c,j;
-printf("Enter 10 numbers respectively: ");
-   for(i=0;i<5;i++){
+int i,x[7],a,b,c,j;
+printf("Enter 7 numbers respectively: ");
+   for(i=0;i<7;i++){
     scanf("%d",&x[i]);
     }
     c=0;a=0;
-    for(j=1;j<5;j++){
+    for(j=1;j<7;j++){
         if(x[j]%x[j-1]==0){
              a=x[j];
             c++;
@@ -25,3 +24,4 @@ printf("Enter 10 numbers respectively: ");
 printf("There are %d number are divisable\n",c);
 
 return 0;}
+
